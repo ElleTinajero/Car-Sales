eefedf3543c3f0bb7c74fccd73f24c517affd6e6
Michelle <Michelle.Tinajero@outlook.com>
2020-04-28 17:10:23 -0500

    connected components

--- src/App.js
+++ src/App.js
@@ -5,50 +5,40 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { addFeature, removeFeature } from './actions/actions';
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
-
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
-
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
-
+const App = ({additionalFeatures, car, additionalPrice, addFeature, removeFeature}) => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={car} />
+        <AddedFeatures car={car} removeFeature={removeFeature} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={additionalFeatures} addFeature={addFeature} />
+        <Total car={car} additionalPrice={additionalPrice} />
       </div>
     </div>
   );
-};
+}
 
+const mapDispatchToProps = {
+  addFeature,
+  removeFeature
+}
 
 // wrap the component export in the connect function
 //pass the component that we want to connect into the second call
 //the first call will taje inb a function and an object
+//function returns an object
+//any property on the returned object gets added to the component props
+const mapStateToProps = (state) => {
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    additionalFeatures: state.additionalFeatures
+  }
+}
 
-export default connect(() => {}, {})(App); // function currying
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default connect(() => {return {} }, {})(App); // function currying
